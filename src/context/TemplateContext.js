e43873ae1409666cdc17163844213b8096ebe6c2
David Lindberg <david.wesley.lindberg@gmail.com>
2023-01-31 13:48:40 -0600

    got location to show up on screen, trying to figure out location tracking

--- src/context/TemplateContext.js
+++ src/context/TemplateContext.js
@@ -1,5 +1,8 @@
 import { useEffect } from "react";
 import { createContext, useState } from "react";
+import * as MediaLibrary from "expo-media-library";
+import * as Location from "expo-location";
+import * as TaskManager from "expo-task-manager";
 
 // create export for context.
 export const TemplateContext = createContext();
@@ -7,17 +10,107 @@ export const TemplateContext = createContext();
 // export the template provider.
 export function TemplateProvider({ children }) {
   // create test data for context.
-  const [userInfo, setUserInfo] = useState({});
+  const [userInfo, setUserInfo] = useState({ id: 1, name: "Your Name" });
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    setUserInfo({
-      id: 1,
-      name: "Your Name",
+  // permissions:
+  const [permission, requestPermission] = MediaLibrary.usePermissions();
+
+  async function getPermissions() {
+    // if (permission === null) {
+    await requestPermission()
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    await Location.requestForegroundPermissionsAsync()
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    await Location.requestBackgroundPermissionsAsync()
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    // }
+  }
+
+  // location services:
+  const [location, setLocation] = useState(null);
+  const [coords, setCoords] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const locations = [];
+
+  async function getLocation() {
+    await Location.getCurrentPositionAsync()
+      .then((res) => {
+        console.log(res);
+        const location = {
+          latitude: res.coords.latitude,
+          longitude: res.coords.longitude,
+        };
+        setCoords(location);
+        Location.reverseGeocodeAsync(location)
+          .then((res) => {
+            console.log(res[0]);
+            setLocation(res[0]);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  TaskManager.defineTask(
+    "LOCATION_TRACKING",
+    ({ data: { locations }, err }) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log("Received new locations", locations);
+    }
+  );
+
+  async function trackLocation() {
+    await Location.startLocationUpdatesAsync("LOCATION_TRACKING", {
+      deferredUpdatesInterval: 2000,
     });
-  }, []);
+  }
+
+  async function stopTracking() {
+    await Location.stopLocationUpdatesAsync("LOCATION_TRACKING");
+  }
+
+  // useEffect(() => {
+  //   setUserInfo({
+  //     id: 1,
+  //     name: "Your Name",
+  //   });
+  // }, []);
 
   return (
-    <TemplateContext.Provider value={{ userInfo }}>
+    <TemplateContext.Provider
+      value={{
+        userInfo,
+        getPermissions,
+        getLocation,
+        location,
+        coords,
+        trackLocation,
+        stopTracking,
+      }}
+    >
       {children}
     </TemplateContext.Provider>
   );
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,16 +1,34 @@
 import { useContext } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, View } from "react-native";
 import TemplateComponent from "../components/TemplateComponent";
 import { TemplateContext } from "../context/TemplateContext";
 
 export default function HomeScreen() {
-  const { userInfo } = useContext(TemplateContext);
+  const {
+    userInfo,
+    getPermissions,
+    getLocation,
+    location,
+    coords,
+    trackLocation,
+    stopTracking,
+  } = useContext(TemplateContext);
 
   return (
     <View style={styles.container}>
       <Text>This is the Home Screen.</Text>
       <Text>Here is some context: {userInfo.name}</Text>
       <TemplateComponent />
+      <Button title="Request Permissions" onPress={() => getPermissions()} />
+      <Button title="Get Location" onPress={() => getLocation()} />
+      <Button title="Start Tracking" onPress={() => trackLocation()}/>
+      {location && (
+        <View>
+          <Text>{`${location.name}, ${location.city}, ${location.region} ${location.postalCode}`}</Text>
+          <Text>{`${coords.latitude}, ${coords.longitude}`}</Text>
+        </View>
+      )}
+      <Button title="Stop Tracking" onPress={() => stopTracking()}/>
     </View>
   );
 }
